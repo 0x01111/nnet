starting to test hinton's mnist dataset assignment using the current implementation. there's something currenly messed up w/ the gradient-descent methods which will hopefully be fixed in the next commit

NeuralNetworkCore.py
@@ -50,11 +50,11 @@ class Network(object):
 			elif method=='alt_random':
 				for i,(n1,n2) in enumerate(zip(self.n_nodes[:-1],self.n_nodes[1:])):
 					v = np.sqrt(6./(n1+n2+1))
-					self.wts_[i] = 2.0*v*np.random.rand(n1+1,n2) - v
+					self.wts_[i] = 2.0*v*np.random.rand(n1+1,n2) - v			
 		else:
 			self.wts_ = wts
 
-	def fit(self,X=None,y=None,x_data=None,method='L-BFGS',n_iter=1500,eps=0.75,alpha=0.9):
+	def fit(self,X=None,y=None,x_data=None,method='L-BFGS',n_iter=1500,learn_rate=0.75,alpha=0.9):
 		'''Fits the weights of the neural network
 
 		Parameters:
@@ -93,22 +93,22 @@ class Network(object):
 		
 		elif method == 'gradient_descent':
 			if not X == None and not y == None:
-				self.wts_ = nopt.gradient_descent(self.wts_,self.update,_X,y,n_iter=n_iter,eps=eps)
+				self.wts_ = nopt.gradient_descent(self.wts_,self.update,_X,y,n_iter=n_iter,learn_rate=learn_rate)
 			elif x_data:
-				self.wts_ = nopt.gradient_descent(self.wts_,self.update,x_data=x_data,n_iter=n_iter,eps=eps)
+				self.wts_ = nopt.gradient_descent(self.wts_,self.update,x_data=x_data,n_iter=n_iter,learn_rate=learn_rate)
 
 		elif method == 'momentum':
 			if not X == None and not y == None:
-				self.wts_ = nopt.momentum(self.wts_,self.update,_X,y,n_iter=n_iter,eps=eps,alpha=alpha)
+				self.wts_ = nopt.momentum(self.wts_,self.update,_X,y,n_iter=n_iter,learn_rate=learn_rate,alpha=alpha)
 			
 			elif x_data:
-				self.wts_ = nopt.momentum(self.wts_,self.update,x_data=x_data,n_iter=n_iter,eps=eps,alpha=alpha)
+				self.wts_ = nopt.momentum(self.wts_,self.update,x_data=x_data,n_iter=n_iter,learn_rate=learn_rate,alpha=alpha)
 
 		elif method == 'improved_momentum':
 			if not _X == None and not y == None:
-				self.wts_ = nopt.improved_momentum(self.wts_,self.update,_X,y,n_iter=n_iter,eps=eps,alpha=alpha)	
+				self.wts_ = nopt.improved_momentum(self.wts_,self.update,_X,y,n_iter=n_iter,learn_rate=learn_rate,alpha=alpha)	
 			elif x_data:
-				self.wts_ = nopt.improved_momentum(self.wts_,self.update,x_data=x_data,n_iter=n_iter,eps=eps,alpha=alpha)
+				self.wts_ = nopt.improved_momentum(self.wts_,self.update,x_data=x_data,n_iter=n_iter,learn_rate=learn_rate,alpha=alpha)
 
 		return self
 	

SoftmaxClassifier.py
@@ -59,11 +59,12 @@ class SoftmaxClassifier(NeuralNetworkCore.Network):
 		return dE_dW[::-1]
 
 	def predict(self,X,y=None):
-		'''One-line description
+		'''Runs forward propagation through the network to predict labels, and computes 
+		the misclassification rate if true labels are provided
 		
 		Parameters:
 		-----------
-		X:
+		X: data matrix 
 		y:
 		
 		Returns:
@@ -72,12 +73,11 @@ class SoftmaxClassifier(NeuralNetworkCore.Network):
 		mce:
 		'''
 		m = X.shape[1]
-		X = np.append(np.ones([1,m]),X,axis=0)
-		self.fprop(X)
+		_X = np.append(np.ones([1,m]),X,axis=0)
+		self.fprop(_X)
 		pred = np.argmax(self.act[-1],axis=0) # only the final activation contains the 
 		
-		if not y:
+		if y==None:
 			return pred
-		mce = 1.0-np.mean(1.0*(pred==np.argmax(y,axis=0)))
-		
+		mce = 1.0-np.mean(1.0*(pred==np.argmax(y,axis=0)))		
 		return pred,mce