fixed the bugs with MNIST.py - need the features to be in range [0,1], otherwise we get underflow issues in the softmax computation - also, updated scipy, so we can now specify the number of iterations for conjugate gradient and l-bfgs. next commit will start doing pre-training to improve classification. 3-layer net on MNIST with 50 hidden units using 100 iterations of lbfgs gives an error rate of ~ 2%. Curious to see how much more pre-training will improve results

NeuralNetworkCore.py
@@ -44,7 +44,7 @@ class Network(object):
 
 			if method=='random':
 				for i,(n1,n2) in enumerate(zip(self.n_nodes[:-1],self.n_nodes[1:])):
-					self.wts_[i] = 0.001*np.random.rand(n1+1,n2)
+					self.wts_[i] = 0.005*np.random.rand(n1+1,n2)
 
 			elif method=='alt_random':
 				for i,(n1,n2) in enumerate(zip(self.n_nodes[:-1],self.n_nodes[1:])):
@@ -90,10 +90,10 @@ class Network(object):
 
 		if method == 'conjugate_gradient':
 			self.fprop(X)
-			self.wts_ = nopt.conjugate_gradient(self.wts_, X, y, self.n_nodes, self.loss, self.loss_grad)
+			self.wts_ = nopt.conjugate_gradient(self.wts_, X, y, self.n_nodes, self.loss, self.loss_grad,n_iter)
 		
 		elif method == 'L-BFGS':
-			self.wts_ = nopt.lbfgs(self.wts_, X, y, self.n_nodes, self.loss, self.loss_grad)
+			self.wts_ = nopt.lbfgs(self.wts_, X, y, self.n_nodes, self.loss, self.loss_grad,n_iter)
 		
 		elif method == 'gradient_descent':
 			if not X == None and not y == None:
@@ -125,7 +125,7 @@ class Network(object):
 		m = _X.shape[1] # number of training cases
 		self.act[0] = self.activ[0](np.dot(wts[0].T,_X)) # use the first data matrix to compute the first activation
 		if len(wts) > 1: # wts = 1 refers to softmax regression
-			self.act[0] = np.vstack(np.ones([1,m]),self.act[0])
+			self.act[0] = np.vstack((np.ones([1,m]),self.act[0]))
 			for i,w in enumerate(wts[1:-1]):
 				self.act[i+1] = np.vstack((np.ones([1,m]),self.activ[i+1](np.dot(w.T,self.act[i])))) # sigmoid activations
 			self.act[-1] = self.activ[-1](np.dot(wts[-1].T,self.act[-2]))
@@ -139,7 +139,8 @@ class Network(object):
 
 		wts = nu.reroll(w,self.n_nodes)
 		self.fprop(_X,wts)
-		return self.cost(y,wts)
+		E = self.cost(y,wts)
+		return E
 
 	def loss_grad(self,w,_X,y):
 		''' convenience grad function for batch optimization methods, e.g., 
@@ -147,7 +148,8 @@ class Network(object):
 		
 		wts = nu.reroll(w,self.n_nodes)
 		grad = self.bprop(_X,y,wts)
-		return nu.unroll(grad)
+		dE = nu.unroll(grad)
+		return dE
 
 	def update(self,_X,y,wts=None):
 		''' convenience function for mini-batch optimization methods, e.g., 

NeuralNetworkCore_test.py
@@ -38,7 +38,7 @@ class testNeuralNetworkCore(unittest.TestCase):
 		# Numerical computation of the gradient..but checking every single derivative is 
 		# cumbersome, check 20% of the values
 		n = sum(np.size(w) for w in nnet.wts_)
- 		idx = np.random.permutation(n)[:(n/20)] # choose a random 20% 
+ 		idx = np.random.permutation(n)[:(n/5)] # choose a random 20% 
  		ngrad = [None]*len(idx)
 
 		for i,x in enumerate(idx):
@@ -65,7 +65,6 @@ class testNeuralNetworkCore(unittest.TestCase):
 
 	def test_SoftmaxClassifier_single_layer(self):
 		''' Gradient checking of backprop for single hidden-layer softmax '''
-
 		n_hid = [50]
 		decay = 0.0
 		nnet = scl.SoftmaxClassifier(d=self.d,k=self.k,n_hid=n_hid,decay=decay)
@@ -87,7 +86,7 @@ class testNeuralNetworkCore(unittest.TestCase):
 		# Numerical computation of the gradient..but checking every single derivative is 
 		# cumbersome, check 20% of the values
 		n = sum(np.size(w) for w in nnet.wts_)
- 		idx = np.random.permutation(n)[:(n/20)] # choose a random 20% 
+ 		idx = np.random.permutation(n)[:(n/5)] # choose a random 20% 
  		ngrad = [None]*len(idx)
 
 		for i,x in enumerate(idx):
@@ -135,7 +134,7 @@ class testNeuralNetworkCore(unittest.TestCase):
 		# Numerical computation of the gradient..but checking every single derivative is 
 		# cumbersome, check 20% of the values
 		n = sum(np.size(w) for w in nnet.wts_)
- 		idx = np.random.permutation(n)[:(n/20)] # choose a random 20% 
+ 		idx = np.random.permutation(n)[:(n/5)] # choose a random 20% 
  		ngrad = [None]*len(idx)
 
 		for i,x in enumerate(idx):

nnetoptim.py
@@ -2,20 +2,20 @@ import numpy as np
 import nnetutils as nu
 from scipy.optimize import fmin_cg,fmin_l_bfgs_b
 
-def conjugate_gradient(wts,_X,y,n_nodes,loss,loss_grad):
+def conjugate_gradient(wts,_X,y,n_nodes,loss,loss_grad,n_iter=100):
 	''' wrapper function of scipy's conjugate gradient method to accept weights as a list of 
 	neural net inter-layer weights'''
 	
 	w0 = nu.unroll(wts)
-	wf = fmin_cg(loss,w0,loss_grad,(_X,y))
+	wf = fmin_cg(loss,w0,loss_grad,(_X,y),maxiter=n_iter)
 	return nu.reroll(wf,n_nodes)
 
-def lbfgs(wts,_X,y,n_nodes,loss,loss_grad):
+def lbfgs(wts,_X,y,n_nodes,loss,loss_grad,n_iter=100):
 	''' wrapper function of scipy's L-BFGS method to accept weights as a list of 
 	neural net inter-layer weights'''
 	
 	w0 = nu.unroll(wts)
-	res = fmin_l_bfgs_b(loss,w0,loss_grad,(_X,y))
+	res = fmin_l_bfgs_b(loss,w0,loss_grad,(_X,y),maxiter=n_iter)
 	return nu.reroll(res[0],n_nodes)
 
 def gradient_descent(wts,update,_X=None, y=None,x_data=None,n_iter=1000,learn_rate=0.35):