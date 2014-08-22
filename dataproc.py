19277d8e683434684128ebba0f6be0dafaabbd17
avasbr <avasbr@avasbr-System-Product-Name.(none)>
2014-08-22 08:32:58 -0400

    added a cross-validation method in the dataproc file - not sure if I should use scikits-learn's version of this or not, I don't think it's a complicated/complex enough method to not just write my own

--- dataproc.py
+++ dataproc.py
@@ -47,3 +47,33 @@ def split_train_validation_test(X,split,method=None):
 def shuffle_data(X):
 	''' Shuffles data '''
 	return X[:,get_subset_idx(X,1.0)]
+
+def cross_val_idx(m,k=10):
+	'''Given the total number of samples, creates training and validation
+	indices to use for cross-validation
+	
+	Parameters
+	----------
+	m:	total number of samples
+		int
+	k:	number of folds
+		int
+	
+	Returns
+	-------
+	train_idx: training indices
+			   list of int lists
+	val_idx: validation indices
+			 list of int lists
+	'''
+	train_idx = [[None] for i in range(k)]
+	val_idx = [[None] for i in range(k)]
+	num_per_fold = m/k
+	idx = list(np.random.permutation(m))
+	for i in range(k):
+		val_idx[i] = idx[i*num_per_fold:(i+1)*num_per_fold]
+		train_idx[i] = list(set(idx)-set(val_idx[i]))
+
+	return train_idx,val_idx
+
+	    
---
+++ dataproc_test.py
@@ -0,0 +1,12 @@
+import unittest
+
+class class_name(unittest.TestCase):
+	def setUp(self):
+
+def main():
+	unittest.main()
+	
+if __name__ == '__main__':
+	main()
+
+    
