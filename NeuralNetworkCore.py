fixed up gradient descent with generators

NeuralNetworkCore.py
@@ -3,7 +3,7 @@
 # cost functions, gradient (bprop) functions, etc should be set-up in the subclasses, because
 # these are what give rise to the variations in neural network architectures. Sparse autoencoders, 
 # RBMs, multilayer softmax nets, even logistic/softmax regression, are all essentially neural
-# networks under the hood. 
+# networks with different cost functions.
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -21,7 +21,6 @@ class Network(object):
 		self.act = (len(self.n_nodes)-1)*[None] # activations for each layer (except input)
 		if all(node for node in self.n_nodes): # triggers only if all values in self.nodes are not of type NoneType
 			self.set_weights(method='alt_random')
-
 
 	def set_weights(self,method='alt_random',wts=None):
 		'''Sets the weights of the neural network based on the specified method
@@ -43,13 +42,13 @@ class Network(object):
 		self.wts_
 
 		'''
-		if not wts:
+		if wts is None:
 			self.wts_ = (len(self.n_nodes)-1)*[None]
 
 			# standard random initialization for neural network weights
 			if method=='random':
 				for i,(n1,n2) in enumerate(zip(self.n_nodes[:-1],self.n_nodes[1:])):
-					self.wts_[i] = 0.005*np.random.rand(n1+1,n2)
+					self.wts_[i] = 0.0005*np.random.rand(n1+1,n2)
 
 			# andrew ng's suggested method in the UFLDL tutorial
 			elif method=='alt_random':
@@ -91,7 +90,7 @@ class Network(object):
 		--------
 		self.wts_	
 		'''
-		if not X:
+		if X is not None:
 			m = X.shape[1]
 			X = np.vstack((np.ones([1,m]),X))
 
@@ -105,22 +104,21 @@ class Network(object):
 			self.wts_ = nu.reroll(wf.x,self.n_nodes)
 		
 		elif method == 'gradient_descent':
-			if not X and not y:
+			if X is not None and y is not None:
 				self.wts_ = nopt.gradient_descent(self.wts_,self.update_network,X,y,n_iter=n_iter,learn_rate=learn_rate)
-			elif x_data:
+			elif x_data is not None:
 				self.wts_ = nopt.gradient_descent(self.wts_,self.update_network,x_data=x_data,n_iter=n_iter,learn_rate=learn_rate)
 
 		elif method == 'momentum':
-			if not X and not y:
+			if X is not None and y is not None:
 				self.wts_ = nopt.momentum(self.wts_,self.update_network,X,y,n_iter=n_iter,learn_rate=learn_rate,alpha=alpha)
-			
-			elif x_data:
+			elif x_data is not None:
 				self.wts_ = nopt.momentum(self.wts_,self.update_network,x_data=x_data,n_iter=n_iter,learn_rate=learn_rate,alpha=alpha)
 
 		elif method == 'improved_momentum':
-			if not X and not y:
+			if X is not None and y is not None:
 				self.wts_ = nopt.improved_momentum(self.wts_,self.update_network,X,y,n_iter=n_iter,learn_rate=learn_rate,alpha=alpha)	
-			elif x_data:
+			elif x_data is not None:
 				self.wts_ = nopt.improved_momentum(self.wts_,self.update_network,x_data=x_data,n_iter=n_iter,learn_rate=learn_rate,alpha=alpha)
 		else:
 			print 'Method does not exist, check your code'
@@ -130,8 +128,9 @@ class Network(object):
 	def compute_activations(self,_X,wts=None):
 		'''Performs forward propagation and computes and stores intermediate activation values'''
 		
-		if not wts:
+		if wts is None:
 			wts = self.wts_
+
 		m = _X.shape[1] # number of training cases
 		ones = np.ones([1,m]) # a row of ones gets appended often, make just once
 		self.act[0] = self.activ[0](np.dot(wts[0].T,_X)) # use the first data matrix to compute the first activation
@@ -157,12 +156,12 @@ class Network(object):
 		''' convenience function for mini-batch optimization methods, e.g., 
 		gradient_descent, momentum, improved_momentum'''
 		
-		if not wts:
+		if wts is None:
 			wts = self.wts_
 		self.compute_activations(_X,wts)
 		dE = self.compute_grad(_X,y,wts)
 		
-		return self.compute_grad(_X,y,wts)
+		return dE
 
 	def reset(self,method='alt_random'):
 		''' resets the weights of the network - useful for re-use'''

nnetoptim.py
@@ -13,8 +13,8 @@ def gradient_descent(wts,update,_X=None, y=None,x_data=None,n_iter=1000,learn_ra
 	x_data:	neural network weight gradients
 				list of numpy ndarrays
 
-	update:	update function which takes current weights and outputs the gradient
-			function
+	update:	update function which takes current weights and outputs the gradient.
+			function handle
 
 	n_iter:	number of iterations
 			int (optional, default = 1000)
@@ -29,21 +29,20 @@ def gradient_descent(wts,update,_X=None, y=None,x_data=None,n_iter=1000,learn_ra
 	'''
 	
 	# full-batch gradient descent
-	if not _X and not y:
+	if _X is not None and y is not None:
 		for i in range(n_iter):
 			grad_wts = update(_X,y,wts)
 			wts = [w-learn_rate*g for w,g in zip(wts,grad_wts)]
-
 		return wts
 		
 	# mini-batch stochastic gradient descent
+	data_gen = x_data()
 	for i in range(n_iter):
-		X,y = x_data.next() # get the next batch of data
+		X,y = data_gen.next() # get the next batch of data
 		m = X.shape[1]
 		X = np.vstack((np.ones([1,m]),X))
 		grad_wts = update(X,y,wts) # run the samples through the network
-		wts = [w-learn_rate*g for w,g in zip(wts,grad_wts)] # update the weights
-		
+		wts = [w-learn_rate*g for w,g in zip(wts,grad_wts)] # update the weights	
 	return wts
 
 def improved_momentum(wts, update, _X=None, y=None,x_data=None,n_iter=1000,learn_rate=0.35,alpha=0.9):
@@ -77,9 +76,8 @@ def improved_momentum(wts, update, _X=None, y=None,x_data=None,n_iter=1000,learn
 	accum_grad_wts = [np.zeros(w.shape) for w in wts] # accumulated gradient
 
 	# full-batch gradient descent with improved momentum
-	if not _X and not y:		
+	if _X is not None and y is not None:		
 		for i in range(n_iter):
-
 			# take a step in the direction of the accumulated gradient first
 			wts = [w + a for w,a in zip(wts,accum_grad_wts)] 
 			grad_wts = update(_X,y,wts) # evaluate the gradient at this new point
@@ -92,10 +90,11 @@ def improved_momentum(wts, update, _X=None, y=None,x_data=None,n_iter=1000,learn
 		return wts
 
 	# mini-batch stochastic gradient descent with improved momentum
+	data_gen = x_data()
 	for i in range(n_iter):
 		
 		# get the next batch of data
-		X,y = x_data.next()
+		X,y = data_gen.next()
 		m = X.shape[1]
 		X = np.vstack((np.ones([1,m]),X))
 
@@ -139,7 +138,7 @@ def momentum(wts,update,_X=None, y=None,x_data=None,n_iter=1000,learn_rate=0.5,a
 	accum_grad_wts = [np.zeros(w.shape) for w in wts] # accumulated gradient
 
 	# full-batch gradient descent with momentum
-	if not _X and not y:
+	if _X is not None and y is not None:
 			
 		for i in range(n_iter):
 		
@@ -152,10 +151,11 @@ def momentum(wts,update,_X=None, y=None,x_data=None,n_iter=1000,learn_rate=0.5,a
 		return wts
 
 	# mini-batch stochastic gradient descent with momentum
+	data_gen = x_data()
 	for i in range(n_iter):
 		
 		# get the next batch of data
-		X,y = x_data.next() 
+		X,y = data_gen.next() 
 		m = X.shape[1]
 		X = np.vstack((np.ones([1,m]),X))
 		grad_wts = update(X,y,wts) # evaluate the gradient at the current point