fixed a bunch of bugs in the autoencoder - MNIST_demo, STL_demo and SAE_demo now all work, and gradient checking has been cleaned up and also verified to work

Autoencoder.py
@@ -24,33 +24,38 @@ class Autoencoder(NeuralNetworkCore.Network):
 		
 		if not wts:
 			wts = self.wts_
-		avg_act = np.mean(self.act[0][1:],axis=1)
+
+		avg_act = np.mean(self.act[0],axis=1)
 		
 		# compute each of the individual costs
 		main_cost = 0.5*np.mean(np.sum((y-self.act[1])**2,axis=0))
-		decay_cost = 0.5*self.decay*sum([np.sum(w[1:]**2) for w in wts])
+		decay_cost = 0.5*self.decay*sum([np.sum(w**2) for w in wts])
 		sparsity_cost = self.beta*np.sum(self.rho*np.log(self.rho/avg_act)+
 			(1-self.rho)*np.log((1-self.rho)/(1-avg_act)))
 		E = main_cost + decay_cost + sparsity_cost
 		return E
 
-	def compute_grad(self,_X,y,wts=None):
+	def compute_grad(self,X,y,wts=None,bs=None):
 		'''Performs back-proparation to compute the gradients with respect to the weights'''				
-		if not wts:
+		if wts is None and bs is None:
 			wts = self.wts_
-		avg_act = np.mean(self.act[0][1:],axis=1)
-		m = _X.shape[1]
-		
-		dE_dW = [None,None] # there can only ever be two weight matrices
+			bs = self.bs_
+
+		avg_act = np.mean(self.act[0],axis=1)
+		m = X.shape[1]
+		# there can only ever be two weight matrices, bias vectors
+		dE_dW = [None,None] 
+		dE_db = [None,None]
+
 		dE_dz = -1.0*(y-self.act[1])*self.act[1]*(1-self.act[1])
-		dE_dW[1] = 1./m*np.dot(self.act[0],dE_dz.T) # gradient of the hidden to output layer weight matrix
-		dE_dW[1][1:] += self.decay*wts[1][1:] # add the gradients of the regularization term
-		dE_da = np.dot(wts[1],dE_dz)[1:] + (self.beta*(-1.0*self.rho/avg_act + (1-self.rho)/(1-avg_act)))[:,np.newaxis]
-		dE_dz = dE_da*self.act[0][1:]*(1-self.act[0][1:])
-		dE_dW[0] = 1./m*np.dot(_X,dE_dz.T) # gradient of the input to hidden layer weight matrix
-		dE_dW[0][1:] += self.decay*wts[0][1:]
+		dE_dW[1] = 1./m*np.dot(dE_dz,self.act[0].T) + self.decay*wts[1] # gradient of the hidden to output layer weight matrix
+		dE_db[1] = 1./m*np.sum(dE_dz,axis=1)
+		dE_da = np.dot(wts[1].T,dE_dz) + (self.beta*(-1.0*self.rho/avg_act + (1-self.rho)/(1-avg_act)))[:,np.newaxis]
+		dE_dz = dE_da*self.act[0]*(1-self.act[0])
+		dE_dW[0] = 1./m*np.dot(dE_dz,X.T) + self.decay*wts[0] # gradient of the input to hidden layer weight matrix
+		dE_db[0] = 1./m*np.sum(dE_dz,axis=1)
 		
-		return dE_dW
+		return dE_dW,dE_db
 
 	def fit(self,X=None,x_data=None,method='L-BFGS',n_iter=None,learn_rate=0.5,alpha=0.9):
 		''' See NeuralNetworkCore,Network.fit for a description of fit. 
@@ -78,10 +83,8 @@ class Autoencoder(NeuralNetworkCore.Network):
 				d x m matrix m = # of training samples, d = # of features		
 		'''
 		
-		m = X.shape[1]
-		X = np.append(np.ones([1,m]),X,axis=0)
 		self.compute_activations(X)
-		X_t = self.act[0][1:]
+		X_t = self.act[0]
 		X_r = self.act[1]
 		
 		if option == 'reduce':
@@ -103,4 +106,4 @@ class Autoencoder(NeuralNetworkCore.Network):
 		d x self.n_hid, d = # of features, self.n_hid = # of hidden units
 		
 		'''
-		return self.wts_[0][1:]/np.sqrt(np.sum(self.wts_[0][1:]**2,axis=0))
+		return self.wts_[0].T/np.sqrt(np.sum(self.wts_[0]**2,axis=1))

NeuralNetworkCore.py
@@ -144,7 +144,7 @@ class Network(object):
 
 		self.act[0] = self.activ[0](np.dot(wts[0],X) + bs[0]) # use the first data matrix to compute the first activation
 		if len(wts) > 1: # len(wts) = 1 corresponds to softmax regression
-			for i,(w,b,activ) in enumerate(wts[1:-1],bs[1:-1],self.activ[1:]):
+			for i,(w,b,activ) in enumerate(zip(wts[1:],bs[1:],self.activ[1:])):
 				self.act[i+1] = activ(np.dot(w,self.act[i]) + b)
 
 	# the following methods are 'conveninence' functions needed for various optimization methods that are called
@@ -152,10 +152,11 @@ class Network(object):
 
 	def compute_cost_grad(self,w,X,y):
 		''' convenience function for scipy.optimize.minimize() '''
-		wts = nu.reroll(w,self.n_nodes)
-		self.compute_activations(X,wts)
+		wts,bs = nu.reroll(w,self.n_nodes)
+		self.compute_activations(X,wts,bs)
 		cost = self.compute_cost(y,wts)
-		grad = nu.unroll(self.compute_grad(X,y,wts))
+		grad_w,grad_b = self.compute_grad(X,y,wts,bs)
+		grad = nu.unroll(grad_w,grad_b)
 
 		return cost,grad
 	
@@ -167,8 +168,8 @@ class Network(object):
 			wts = self.wts_
 			bs = self.bs_
 
-		self.compute_activations(X,wts)
-		dE = self.compute_grad(X,y,wts)
+		self.compute_activations(X,wts,bs)
+		dE = self.compute_grad(X,y,wts,bs)
 		
 		return dE
 

NeuralNetworkCore_test.py
@@ -13,151 +13,90 @@ class testNeuralNetworkCore(unittest.TestCase):
 		self.k = 3
 		self.m = 100
 
-		self._X = np.random.rand(self.d+1,self.m) # generate some synthetic data (5-dim feature vectors)
-		self._X[0] = 1 # first row is a bias term, so replace with all 1's
+		self.X = np.random.rand(self.d,self.m) # generate some synthetic data (5-dim feature vectors)
 		self.y = np.zeros((self.k,self.m))
 		for i,v in enumerate(np.random.randint(0,self.k,self.m)):
 			self.y[v,i] = 1 # create one-hot vectors
+
+	def gradient_checker(self,w0,b0,X,y,network):
+		
+		network.compute_activations(X,wts=w0,bs=b0)
+		grad_w0,grad_b0 = network.compute_grad(X,y,wts=w0,bs=b0)	
+		bgrad = nu.unroll(grad_w0,grad_b0)
+		
+		err_tol = 1e-9
+		eps = 1e-4
+
+		n = np.size(bgrad)
+		idx = np.random.permutation(n)[:(n/5)] # choose a random 20% 
+ 		ngrad = [None]*len(idx)
+
+		for i,x in enumerate(idx):
+			wb_plus = nu.unroll(w0,b0)
+			wb_minus = nu.unroll(w0,b0)
+			
+			# Perturb one of the weights by eps
+			wb_plus[x] += eps
+			wb_minus[x] -= eps
+			wts_plus, bs_plus = nu.reroll(wb_plus,network.n_nodes)
+			wts_minus, bs_minus = nu.reroll(wb_minus,network.n_nodes)
+			
+			# run fprop and compute the loss for both sides  
+			network.compute_activations(X,wts_plus,bs_plus) # this modifies the activations
+			loss_plus = network.compute_cost(y,wts_plus)
+			network.compute_activations(X,wts_minus,bs_minus)
+			loss_minus = network.compute_cost(y,wts_minus)
+			
+			ngrad[i] = 1.0*(loss_plus-loss_minus)/(2*eps) # ( E(weights[i]+eps) - E(weights[i]-eps) )/(2*eps)
+			
+		# Compute difference between numerical and backpropagated derivatives
+		cerr = np.mean(np.abs(ngrad-bgrad[idx]))
+		self.assertLess(cerr,err_tol)
 
 	def test_Autoencoder(self):
 
 		n_hid = 50
 		nnet = ae.Autoencoder(d=self.d,n_hid=n_hid)
 				
-		nnet.wts_ = []
+		wts = []
+		bs = []
+
 		for n1,n2 in zip(nnet.n_nodes[:-1],nnet.n_nodes[1:]):
-			nnet.wts_.append(0.01*np.cos(np.arange((n1+1)*n2)).reshape(n1+1,n2))
-		
-		nnet.compute_activations(self._X)
-		grad = nnet.compute_grad(self._X,self._X[1:])
-		bgrad = nu.unroll(grad)
+			wts.append(np.random.randn(n2,n1))
+			bs.append(np.random.randn(n2,1))
 
-		err_tol = 1e-10	# tolerance
-		eps = 1e-4	# epsilon (for numerical gradient computation)
+		self.gradient_checker(wts,bs,self.X,self.X,nnet)
 
-		# Numerical computation of the gradient..but checking every single derivative is 
-		# cumbersome, check 20% of the values
-		n = sum(np.size(w) for w in nnet.wts_)
- 		idx = np.random.permutation(n)[:(n/5)] # choose a random 20% 
- 		ngrad = [None]*len(idx)
+	def test_softmax_single_layer(self):
 
-		for i,x in enumerate(idx):
-			w_plus = nu.unroll(nnet.wts_)
-			w_minus = nu.unroll(nnet.wts_)
-			
-			# Perturb one of the weights by eps
-			w_plus[x] += eps
-			w_minus[x] -= eps
-			weights_plus = nu.reroll(w_plus,nnet.n_nodes)
-			weights_minus = nu.reroll(w_minus,nnet.n_nodes)
-			
-			# run fprop and compute the loss for both sides  
-			nnet.compute_activations(self._X,weights_plus)
-			loss_plus = nnet.compute_cost(self._X[1:],weights_plus)
-			nnet.compute_activations(self._X,weights_minus)
-			loss_minus = nnet.compute_cost(self._X[1:],weights_minus)
-			
-			ngrad[i] = 1.0*(loss_plus-loss_minus)/(2*eps) # ( E(weights[i]+eps) - E(weights[i]-eps) )/(2*eps)
-			
-		# Compute difference between numerical and backpropagated derivatives
-		cerr = np.mean(np.abs(ngrad-bgrad[idx]))
-		self.assertLess(cerr,err_tol)
-
-	def test_SoftmaxClassifier_single_layer(self):
-		''' Gradient checking of backprop for single hidden-layer softmax '''
 		n_hid = [50]
-		decay = 0.0
+		decay = 0.1
 		nnet = scl.SoftmaxClassifier(d=self.d,k=self.k,n_hid=n_hid,decay=decay)
+				
+		wts = []
+		bs = []
 
-		# initialize weights deterministically
-		n_nodes = [self.d]+n_hid+[self.k] # concatenate the input and output layers
-		
-		nnet.wts_ = []
-		for n1,n2 in zip(n_nodes[:-1],n_nodes[1:]):
-			nnet.wts_.append(0.01*np.cos(np.arange((n1+1)*n2)).reshape(n1+1,n2))
+		for n1,n2 in zip(nnet.n_nodes[:-1],nnet.n_nodes[1:]):
+			wts.append(np.random.randn(n2,n1))
+			bs.append(np.random.randn(n2,1))
 
-		nnet.compute_activations(self._X) # fprop through the network, sets activations
-		grad = nnet.compute_grad(self._X,self.y) # back-prop 
-		bgrad = nu.unroll(grad)
-
-		err_tol = 1e-10	# tolerance
-		eps = 1e-5	# epsilon (for numerical gradient computation)
-
-		# Numerical computation of the gradient..but checking every single derivative is 
-		# cumbersome, check 20% of the values
-		n = sum(np.size(w) for w in nnet.wts_)
- 		idx = np.random.permutation(n)[:(n/5)] # choose a random 20% 
- 		ngrad = [None]*len(idx)
-
-		for i,x in enumerate(idx):
-			w_plus = nu.unroll(nnet.wts_)
-			w_minus = nu.unroll(nnet.wts_)
-			
-			# Perturb one of the weights by eps
-			w_plus[x] += eps
-			w_minus[x] -= eps
-			weights_plus = nu.reroll(w_plus,n_nodes)
-			weights_minus = nu.reroll(w_minus,n_nodes)
-			
-			# run fprop and compute the loss for both sides  
-			nnet.compute_activations(self._X,weights_plus)
-			loss_plus = nnet.compute_cost(self.y, weights_plus)
-			nnet.compute_activations(self._X,weights_minus)
-			loss_minus = nnet.compute_cost(self.y, weights_minus)
-			
-			ngrad[i] = 1.0*(loss_plus-loss_minus)/(2*eps) # ( E(weights[i]+eps) - E(weights[i]-eps) )/(2*eps)
-			
-		# Compute difference between numerical and backpropagated derivatives
-		cerr = np.mean(np.abs(ngrad-bgrad[idx]))
-		self.assertLess(cerr,err_tol)
-
-	def test_SoftmaxClassifier_multi_layer(self):
+		self.gradient_checker(wts,bs,self.X,self.y,nnet)
+	
+	def test_softmax_multilayer(self):
 		''' Gradient checking of backprop for multi-hidden-layer softmax '''
 
 		n_hid = [50,25]
-		nnet = scl.SoftmaxClassifier(d=self.d,k=self.k,n_hid=n_hid)
-
-		# initialize weights deterministically
-		n_nodes = [self.d]+n_hid+[self.k] # concatenate the input and output layers
+		decay = 0.2
+		nnet = scl.SoftmaxClassifier(d=self.d,k=self.k,n_hid=n_hid,decay=decay)
 		
-		nnet.wts_ = []
-		for n1,n2 in zip(n_nodes[:-1],n_nodes[1:]):
-			nnet.wts_.append(0.01*np.cos(np.arange((n1+1)*n2)).reshape(n1+1,n2))
+		wts = []
+		bs = []
 
-		nnet.compute_activations(self._X)
-		grad = nnet.compute_grad(self._X,self.y)
-		bgrad = nu.unroll(grad)
+		for n1,n2 in zip(nnet.n_nodes[:-1],nnet.n_nodes[1:]):
+			wts.append(np.random.randn(n2,n1))
+			bs.append(np.random.randn(n2,1))
 
-		err_tol = 1e-10	# tolerance
-		eps = 1e-5	# epsilon (for numerical gradient computation)
-
-		# Numerical computation of the gradient..but checking every single derivative is 
-		# cumbersome, check 20% of the values
-		n = sum(np.size(w) for w in nnet.wts_)
- 		idx = np.random.permutation(n)[:(n/5)] # choose a random 20% 
- 		ngrad = [None]*len(idx)
-
-		for i,x in enumerate(idx):
-			w_plus = nu.unroll(nnet.wts_)
-			w_minus = nu.unroll(nnet.wts_)
-			
-			# Perturb one of the weights by eps
-			w_plus[x] += eps
-			w_minus[x] -= eps
-			weights_plus = nu.reroll(w_plus,n_nodes)
-			weights_minus = nu.reroll(w_minus,n_nodes)
-			
-			# run fprop and compute the loss for both sides  
-			nnet.compute_activations(self._X,weights_plus)
-			loss_plus = nnet.compute_cost(self.y,weights_plus)
-			nnet.compute_activations(self._X,weights_minus)
-			loss_minus = nnet.compute_cost(self.y,weights_minus)
-			
-			ngrad[i] = 1.0*(loss_plus-loss_minus)/(2*eps) # ( E(weights[i]+eps) - E(weights[i]-eps) )/(2*eps)
-			
-		# Compute difference between numerical and backpropagated derivatives
-		cerr = np.mean(np.abs(ngrad-bgrad[idx]))
-		self.assertLess(cerr,err_tol)
+		self.gradient_checker(wts,bs,self.X,self.y,nnet)
 
 def main():
 	unittest.main()

SoftmaxClassifier.py
@@ -25,38 +25,41 @@ class SoftmaxClassifier(NeuralNetworkCore.Network):
 			wts = self.wts_
 
 		#  E = 1/N*sum(-y*log(p)) - negative log probability of the right answer
-		E = np.mean(np.sum(-1.0*y*np.log(self.act[-1]),axis=0)) + 0.5*self.decay*sum([np.sum(w[1:]**2) for w in wts])
+		E = np.mean(np.sum(-1.0*y*np.log(self.act[-1]),axis=0)) + 0.5*self.decay*sum([np.sum(w**2) for w in wts])
 
 		return E
 
-	def compute_grad(self,_X,y,wts=None):
+	def compute_grad(self,X,y,wts=None,bs=None):
 		'''Back-propagation for L2-regularized cross-entropy cost function'''
 
-		if wts is None:
+		if wts is None and bs is None:
 			wts = self.wts_
+			bs = self.bs_
 
 		# reversing the lists makes it easier 					
 		wts = wts[::-1]
+		bs = bs[::-1]
 		act = self.act[::-1]
 
-		m = _X.shape[1]
 		dE_dW = len(wts)*[None]
-		
+		dE_db = len(bs)*[None]
+
 		# the final layer is a softmax, so calculate the derivative with respect to 
 		# the inputs to the softmax first
 		dE_dz = act[0]-y
 		
+		m = X.shape[1]
 		if len(wts)>1: # wts = 1 means there's no hidden layer = softmax regression
 			for i,a in enumerate(act[1:]):
-				dE_dW[i] = 1./m*np.dot(a,dE_dz.T)
-				dE_dW[i][1:] += self.decay*wts[i][1:]
-				dE_da = np.dot(wts[i],dE_dz)
-				dE_dz = (dE_da*a*(1-a))[1:] # no connection to the bias node
-		dE_dW[-1] = 1./m*np.dot(_X,dE_dz.T)
-		dE_dW[-1][1:] += self.decay*wts[-1][1:]
+				dE_dW[i] = 1./m*np.dot(dE_dz,a.T) + self.decay*wts[i]
+				dE_db[i] = 1./m*np.sum(dE_dz,axis=1)[:,np.newaxis]
+				dE_da = np.dot(wts[i].T,dE_dz)
+				dE_dz = dE_da*a*(1-a) # no connection to the bias node
+		dE_dW[-1] = 1./m*np.dot(dE_dz,X.T) + self.decay*wts[-1]
+		dE_db[-1] = 1./m*np.sum(dE_dz,axis=1)[:,np.newaxis]
 
 		# re-reverse and return
-		return dE_dW[::-1]
+		return dE_dW[::-1],dE_db[::-1]
 
 	def predict(self,X,y=None):
 		'''Runs forward propagation through the network to predict labels, and computes 
@@ -76,8 +79,6 @@ class SoftmaxClassifier(NeuralNetworkCore.Network):
 		mce: misclassification error, if labels were provided
 			 float
 		'''
-		m = X.shape[1]
-		X = np.append(np.ones([1,m]),X,axis=0)
 		self.compute_activations(X)
 		pred = np.argmax(self.act[-1],axis=0) # only the final activation contains the 
 		

nnetutils.py
@@ -38,14 +38,11 @@ def reroll(v,n_nodes):
 	r_wts = []
 	r_bs = []
 	for col,row in zip(n_nodes[:-1],n_nodes[1:]):
-		w_size = row*col
-		b_size = col
-		r_wts.append(np.reshape(v[idx:idx+w_size],(row,col)))
-		idx += w_size
-		r_bs.append(np.reshape(v[idx:idx+b_size],(col,1)))
-		idx += b_size
+		w_size = row*col; b_size = row
+		r_wts.append(np.reshape(v[idx:idx+w_size],(row,col))); idx += w_size
+		r_bs.append(np.reshape(v[idx:idx+b_size],(row,1))); idx += b_size
 	
-	return r_wts,b_wts
+	return r_wts,r_bs
 
 def clamp(a,minv,maxv):
 	''' imposes a range on all values of a matrix '''